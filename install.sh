c1e1257b1497d53bd4a04b6994dea4d3b17bf201
gconeice <yyang811@gatech.edu>
2023-11-07 13:59:30 -0500

    update install sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-mkdir build && cd build && CC=clang CXX=clang++ cmake ../ && make && cp ../sha256.txt ./ && mkdir data
+mkdir build && cd build && CC=clang CXX=clang++ cmake ../ && make && mkdir data
